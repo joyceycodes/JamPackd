b5c1b19b7d2404be95812b01a682a3bbd95cfed5
Tiffany <tiffany.ameral@gmail.com>
2022-11-16 13:01:35 -0800

    minor changes again

--- accounts/main.py
+++ accounts/main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
 
-<<<<<<< HEAD
-# from routers import users
-
-
 app = FastAPI()
 
 
-=======
-app = FastAPI()
-
-
->>>>>>> main
 @app.get("/api/test")
 def test():
     return "Hello FastAPI"
