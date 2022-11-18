4c72ade598d5bd9610a12d809f5fd23efcfb4ec5
Marble <scnideffer@gmail.com>
2022-11-18 15:17:58 -0800

    did some work on playlists.py routers

--- music/routers/playlists.py
+++ music/routers/playlists.py
@@ -4,21 +4,46 @@ from pydantic import BaseModel
 
 router = APIRouter()
 
+
 class Playlist(BaseModel):
+    _id: int
     name: str
-    user_id: int
+    songs: list
+    num_songs: len(songs)
+
+
+class PlaylistIn(BaseModel):
+    _id: int
+    name: str
+    songs: list
+    num_songs: len(songs)
+
+
+class PlaylistOut(BaseModel):
+    _id: int
+    name: str
+    songs: list
+    num_songs: len(songs)
+
 
 class PlaylistsOut(BaseModel):
     playlists: list[Playlist]
 
+
 # get all playlists
 @router.get("/api/playlists", response_model=PlaylistsOut)
 def list_playlists(queries: PlaylistQueries = Depends()):
-    return{
+    return {
         "playlists": queries.get_all_playlists(),
     }
 
+
 # get playlist by ID
 @router.get("/api/playlists/{id}", response_model=Playlist)
-def get_playlist_detail(int: id):
+def get_playlist_detail():
     return Playlist
+
+
+@router.get("/api/recommendations")
+def get_recommendations():
+    pass
