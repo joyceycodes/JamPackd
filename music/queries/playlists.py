8001a452395a337aaad92a646474c7d78fe679fc
Joyce Yu <joyceyu96@gmail.here>
2022-11-18 15:46:17 -0800

    Localhost:8003/docs working

--- music/queries/playlists.py
+++ music/queries/playlists.py
@@ -1,20 +1,22 @@
 import os
-import pymongo
 
-dbhost = os.environ["MONGOHOST"]
-dbname = os.environ["MONGODATABASE"]
-dbuser = os.environ["MONGOUSER"]
-dbpass = os.environ["MONGOPASSWORD"]
+# import pymongo
 
-mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+# dbhost = os.environ["MONGOHOST"]
+# dbname = os.environ["MONGODATABASE"]
+# dbuser = os.environ["MONGOUSER"]
+# dbpass = os.environ["MONGOPASSWORD"]
 
-client = pymongo.MongoClient(mongo_str)
+# mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+
+# client = pymongo.MongoClient(mongo_str)
 
 
 class PlaylistQueries:
-    def get_all_playlists(self):
-        db = client[dbname]
-        result = list(db.users.find())
-        for value in result:
-            value["id"] = value["_id"]
-        return result
+    # def get_all_playlists(self):
+    #     db = client[dbname]
+    #     result = list(db.users.find())
+    #     for value in result:
+    #         value["id"] = value["_id"]
+    #     return result
+    pass
