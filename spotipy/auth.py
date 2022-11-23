a1f9dde6a8db0b7aa4c021a0ea46eae1173e976c
Tiffany <tiffany.ameral@gmail.com>
2022-11-22 18:09:59 -0800

    Worked w/ Joyce to get spotipy stuff up and running sucessfuly

--- spotipy/auth.py
+++ spotipy/auth.py
@@ -1,11 +1,38 @@
 import spotipy
+import key
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+scope = ["playlist-modify-private", "playlist-modify-public"]
+uris = ["spotify:track:1LDeuD3jbSpHucsd0nOt6t"]
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results["items"]):
-    track = item["track"]
-    print(idx, track["artists"][0]["name"], " – ", track["name"])
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=key.client_ID,
+        client_secret=key.client_SECRET,
+        redirect_uri=key.redirect_url,
+        scope=scope,
+    )
+)
+
+
+pl_name = input("Enter Playlist Name: ")
+
+# results = sp.user_playlists(key.user_joyce, limit=50, offset=0)
+# results = sp.user_playlist_add_tracks(
+#     key.user, playlist_id="3AUA37TYmufE9fVyrwz8aW", tracks=uris
+# )
+
+
+# create playlist
+results = sp.user_playlist_create(
+    key.user, pl_name, public=False, collaborative=True, description=""
+)
+
+# results = sp.user("1254524921")
+print("Results********", results["id"])
+
+
+# for idx, item in enumerate(results["items"]):
+#     track = item["track"]
+#     print(idx, track["artists"][0]["name"], " – ", track["name"])
