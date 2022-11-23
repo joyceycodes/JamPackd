16549a30e9fd2b9f580a64f21908446b28f904cf
Marble <scnideffer@gmail.com>
2022-11-22 18:22:55 -0800

    front page created, not linked. refactored ghi folder for our needs

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './delete question mark/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,18 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-var MyClass = React.createClass({
-  render: function() {
-    return (
-      let <div>
-        <a href="#" style={className clr='#fffd00'}><span>highlighter yellow</span><i /></a>
-        <a href="#" style={{--clr: '#00e9ff'}}><span>teal</span><i /></a>
-        <a href="#" style={{--clr: '#b800ff'}}><span>neon purple</span><i /></a>
-      </div>
-    );
-  }
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
