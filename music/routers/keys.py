c1a925ef5c0771fa3310901b6d98f202bc641f3f
Joyce Yu <joyceyu96@gmail.here>
2022-12-07 18:32:27 -0800

    deleted extra file

--- music/routers/keys.py
+++
@@ -1,4 +0,0 @@
-client_ID = "26c21b44542b466295adedc6ee996cb2"
-client_SECRET = "1144c6e7b2564a019d531d536d5e9d5e"
-redirect_uri = "https://jampackd-music.onrender.com/api/spotifytoken"
-user = "31gv6grbwedyw3u7fxfucsuvq7ua"
