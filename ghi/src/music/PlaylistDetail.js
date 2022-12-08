9199dee78263ffa1b4ff54223ee78fb37d12f70e
Marble <scnideffer@gmail.com>
2022-12-08 13:56:57 -0800

    able to view ALL playlists on homepage, need to filter to only user's created playlists

--- ghi/src/music/PlaylistDetail.js
+++ ghi/src/music/PlaylistDetail.js
@@ -1,11 +1,16 @@
-
-import DeleteButton from "./DeleteButton";
-
 import DeleteButton from "./DeleteButton";
 import React, { useState, useEffect } from "react";
 // import SpotifyButton from "./SpotifyExport";
 import { useParams } from "react-router-dom";
 import { useAuthContext } from "../accounts/auth.js";
+
+
+// let mappedPlaylist = playlist?.map(x => {
+//     return (
+//         playlist.name
+//     )
+// })
+
 
 
 function PlaylistDetail() {
@@ -58,78 +63,34 @@ function PlaylistDetail() {
                             </tr>
                         </thead>
                         <tbody>
-
-                            {pres_songs.map(song => {
+                            {playlist.songs?.map(song => {
                                 return (
                                     <tr key={song.uri}>
-                                        <td> {song.uri} </td>
+                                        <td>{song.name}</td>
+                                        <td>{song.artist}</td>
+                                        <td>
+                                            <iframe title="Current Song" className='container-sm justify-content-center' allow='encrypted-media'
+                                                src={`https://open.spotify.com/embed/track/${song.uri}?utm_source=oembed`} >
+                                            </iframe>
+                                        </td>
                                     </tr>
                                 )
-                            })};
-                            <td>
-                                {/* {pres_songs} */}
-                                {/* {pres_songs.title}
-                                <iframe title="Current Song" className='container-sm justify-content-center' allow='encrypted-media'
-                                    src={`https://open.spotify.com/embed/track/${pres_songs.uri}?utm_source=oembed`} >
-                                </iframe> */}
-                            </td>
+                            })}
                         </tbody>
+
                     </table>
                 </div>
-
-
-
+            </div>
+            {/* <SpotifyButton /> */}
+            <br />
+            <DeleteButton setIsDeleted={setIsDeleted} />
+            <div className={isDeleted ? "alert alert-success mb-0 mt-3" : "alert alert-success d-none mb-0"} id="delete-message">
+                Playlist has been deleted.
             </div>
 
-
-
-            {/* {console.log(songs)} */}
-            {console.log(pres_songs)}
-
-            <div className='mt-5 container-sm border 
-            border-secondary rounded bold justify-content-center'>
-
-                <h1 className="text-center">My Playlist- Pass in Dynamically Later</h1>
-                <div className='mt-5 container-sm border 
-            border-secondary rounded bold justify-content-center'>
-                    <table className="table table-striped 
-                    justify-content-center">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Artist</th>
-                                <th>Player</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            {pres_songs.map(song => {
-                                return (
-                                    <tr key={song.uri}>
-                                        <td> {song.uri} </td>
-                                    </tr>
-                                )
-                            })};
-                            <td>
-                                {/* {pres_songs} */}
-                                {/* {pres_songs.title}
-                                <iframe title="Current Song" className='container-sm justify-content-center' allow='encrypted-media'
-                                    src={`https://open.spotify.com/embed/track/${pres_songs.uri}?utm_source=oembed`} >
-                                </iframe> */}
-                            </td>
-                        </tbody>
-                    </table>
-                </div>
-
-
-
-            </div>
-
-
-
-            <SpotifyButton />
         </>
     )
 }
+
 
 export default PlaylistDetail;
