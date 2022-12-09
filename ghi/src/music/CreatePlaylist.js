521ddd9e6cdced5135b874f624e259010a725726
Joyce Yu <joyceyu96@gmail.com>
2022-12-09 17:50:51 +0000

    Added user_id to endpoints and updated mongo string in queries

--- ghi/src/music/CreatePlaylist.js
+++ ghi/src/music/CreatePlaylist.js
@@ -10,7 +10,6 @@ function CreatePlaylist() {
     const [comments, setComments] = useState("");
     const songs = JSON.parse(window.localStorage.getItem("uris"))
 
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         const data = {
@@ -18,7 +17,7 @@ function CreatePlaylist() {
             comments,
             songs,
         }
-
+        console.log(data)
         const url = `${process.env.REACT_APP_MUSIC}/api/playlists/`
 
         const fetchConfig = {
@@ -30,9 +29,9 @@ function CreatePlaylist() {
             },
         };
         const response = await fetch(url, fetchConfig)
+        console.log(response)
         if (response.ok) {
             const newPlaylist = await response.json();
-
             navigate(`/music/playlist/${newPlaylist.id}`)
         }
     }
@@ -50,7 +49,7 @@ function CreatePlaylist() {
                                 <label htmlFor="name">Name</label>
                             </div>
                             <div className="mb-3 form-floating">
-                                <textarea onChange={(e) => setComments(e.target.value)} placeholder="Comments" required name="comments" id="comments" className="form-control" />
+                                <textarea onChange={(e) => setComments(e.target.value)} placeholder="Comments" required name="comments" id="comments" className="form-control" rows="5" />
                                 <label htmlFor="name">Add your thoughts about this playlist...</label>
                             </div>
                             <button className="btn btn-outline-dark">Submit</button>
