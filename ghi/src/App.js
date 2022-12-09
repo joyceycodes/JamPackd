bfe571766be5b2c1c7a51da58bd0e65c295a0858
Zoybh <husseinzoybh@gmail.com>
2022-12-09 09:59:30 -0800

    Fixed  when logged in you dont see signup button

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './css/App.css';
+// import './css/App.css';
 import MainPage from './mainpage'
 import SignupComponent from "./accounts/signup"
 import LoginComponent from "./accounts/login"
@@ -10,37 +10,60 @@ import Navigation from './nav';
 import PlaylistDetail from './music/PlaylistDetail';
 import CreatePlaylist from './music/CreatePlaylist';
 import UpdatePlaylist from './music/UpdatePlaylist';
+import { useAuthContext } from "./accounts/auth";
+
 
 
 const domain = /https:\/\/[^/]+/;
 const basename = process.env.PUBLIC_URL.replace(domain, '');
 
 function App() {
+  const { token } = useAuthContext();
 
-  return (
-    <BrowserRouter basename={basename}>
-      <Navigation />
-      <div >
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="accounts">
-            <Route path="/accounts/login" element={<LoginComponent LoginComponent={LoginComponent} />} />
-            <Route path="/accounts/signup" element={<SignupComponent SignupForm={SignupComponent} />} />
-            <Route path="/accounts/accountpage" element={<AccountPageComponent AccountPageComponent={AccountPageComponent} />} />
-          </Route>
-          <Route path="music">
-            <Route path="/music/recommendations" element={<RecommendationsForm />} />
-            <Route path="/music/playlist/new" element={<CreatePlaylist />} />
-            <Route path="/music/playlist/:playlist_id" element={<PlaylistDetail />} />
-            <Route path="/music/playlist/update/:playlist_id" element={<UpdatePlaylist />} />
-          </Route>
-          {/* /* <Route path="/new_playlist" element={} */}
-        </Routes >
-      </div >
 
-    </BrowserRouter >
-  );
+  if (!token) {
+    return (
+      <div>
+
+        <BrowserRouter basename={basename}>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="accounts">
+              <Route path="/accounts/signup" element={<SignupComponent SignupForm={SignupComponent} />} />
+              <Route path="/accounts/login" element={<LoginComponent LoginComponent={LoginComponent} />} />
+            </Route>
+          </Routes>
+        </BrowserRouter >
+      </div>
+
+    )
+
+  } else {
+    return (
+      <div>
+
+        <BrowserRouter basename={basename}>
+          <Navigation />
+          <Routes>
+            <Route path="accounts">
+              <Route path="/accounts/accountpage" element={<AccountPageComponent AccountPageComponent={AccountPageComponent} />} />
+            </Route>
+            <Route path="music">
+              <Route path="/music/recommendations" element={<RecommendationsForm />} />
+              <Route path="/music/playlist/new" element={<CreatePlaylist />} />
+              <Route path="/music/playlist/:playlist_id" element={<PlaylistDetail />} />
+              <Route path="/music/playlist/update/:playlist_id" element={<UpdatePlaylist />} />
+            </Route>
+          </Routes>
+        </BrowserRouter >
+      </div>
+
+    )
+
+  }
 }
+
 
 
 export default App;
