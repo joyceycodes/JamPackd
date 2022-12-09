d2f24e197e5974dcc2d02de786666cb28228da29
Joyce Yu <joyceyu96@gmail.here>
2022-12-09 12:32:45 -0800

    finished unit test for get all playlists

--- music/tests/test_music.py
+++ music/tests/test_music.py
@@ -1,19 +1,31 @@
 from fastapi.testclient import TestClient
 from queries.playlists import PlaylistQueries
 from main import app
+from authenticator_music import authenticate
 
 client = TestClient(app)
 
 
 class EmptyPlaylistQueries:
-    def get_all_playlists(self):
+    def get_all_playlists(sel, user_id):
         return []
 
 
 def test_get_all_playlists():
+
+    # Arrange
+    account = {"id": 123}
     app.dependency_overrides[PlaylistQueries] = EmptyPlaylistQueries
+    app.dependency_overrides[
+        authenticate.get_current_account_data
+    ] = lambda: account
+
+    # Act
     response = client.get("/api/playlists")
+
+    # Assert
     assert response.status_code == 200
     assert response.json() == {"playlists": []}
 
+    # Clean up
     app.dependency_overrides = {}
