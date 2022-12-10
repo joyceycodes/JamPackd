da4623fff1ed57d588bbc8337e42286fafe4d2cf
Tiffany <tiffany.ameral@gmail.com>
2022-12-09 18:51:34 -0800

    Unit test added

--- music/tests/test_music.py
+++ music/tests/test_music.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from queries.playlists import PlaylistQueries
+from authenticator_music import authenticate
 from main import app
 
 client = TestClient(app)
@@ -9,11 +10,76 @@ class EmptyPlaylistQueries:
     def get_all_playlists(self):
         return []
 
+    def delete_playlist(self, id):
 
-def test_get_all_playlists():
+        return True
+
+
+# def test_get_all_playlists():
+#     app.dependency_overrides[PlaylistQueries] = EmptyPlaylistQueries
+#     response = client.get("/api/playlists")
+#     assert response.status_code == 200
+#     assert response.json() == {"playlists": []}
+
+#     app.dependency_overrides = {}
+
+
+def test_delete_playlist():
+
+    account = {"id": 123}
     app.dependency_overrides[PlaylistQueries] = EmptyPlaylistQueries
-    response = client.get("/api/playlists")
+    app.dependency_overrides[
+        authenticate.get_current_account_data
+    ] = lambda: account
+
+    response = client.delete("api/playlists/1")
+
     assert response.status_code == 200
-    assert response.json() == {"playlists": []}
+    assert response.json() == True  # noqa
 
     app.dependency_overrides = {}
+
+
+# class MockPlaylistQueries:
+#     def create_playlist(self, data, user_id):
+#         playlist_good = {
+#         {
+#             "id": 0,
+#             "name": "Test Playlist",
+#             "songs": [{"name": "string",
+# "artist": "string", "uri": "string"}],
+#             "comments": "Tasty comments here",
+#         },
+#         }
+#         playlist_good.get_playlist(data, user_id)
+#         return playlist_good
+
+
+#     def test_create_playlist():
+#         json = PlaylistIn(
+#             id="",
+#             name = "Test Playlist",
+#             songs: [],
+#             comments = "Tasty comments here"
+#         )
+
+#     expected = {
+
+#     }
+
+
+# playlist_good = {
+#     {
+#         "id": 0,
+#         "name": "Test Playlist",
+#         "songs": [{"name": "string", "artist": "string", "uri": "string"}],
+#         "comments": "Tasty comments here",
+#     }
+# }
+
+
+# def test_create_playlist():
+#     app.dependency_overrides[PlaylistQueries] = MockPlaylistQueries
+#     reponse = client.post("/api/playlists")
+#     assert reponse.status_code == 200
+#     assert reponse.json() == playlist_good
