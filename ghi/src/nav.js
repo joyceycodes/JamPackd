94b22a4759b6073afc76989c4cde3ad148626194
Marble <scnideffer@gmail.com>
2022-12-12 09:42:55 -0800

    added img alt prop in nav.js

--- ghi/src/nav.js
+++ ghi/src/nav.js
@@ -4,7 +4,7 @@ import LogoutTest from './accounts/newlogout';
 function Navigation() {
   return (
     <div className='background'>
-      <div className="logo-container"><NavLink to="/"><img className="JP_logo" src="https://i.imgur.com/HxIEBd3.png"></img></NavLink></div>
+      <div className="logo-container"><NavLink to="/"><img className="JP_logo" src="https://i.imgur.com/HxIEBd3.png" alt="JamPackd Logo"></img></NavLink></div>
       <div className="nav-container">
         <div className="nav-tabs">
           <ul>
