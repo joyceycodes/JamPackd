b1f77ce0676cfc8f52daf028dbbd1977b8ecbb31
Tiffany <tiffany.ameral@gmail.com>
2022-12-12 10:59:33 -0800

    Went back and re-added unit test, added

--- music/tests/test_music.py
+++ music/tests/test_music.py
@@ -26,10 +26,24 @@ class EmptyPlaylistQueries:
 
 def test_delete_playlist():
 
+    account = {"id": 123}
+    app.dependency_overrides[PlaylistQueries] = EmptyPlaylistQueries
+    response = client.get("/api/playlists")
+    app.dependency_overrides[
+        authenticate.get_current_account_data
+    ] = lambda: account
+
+    response = client.delete("api/playlists/1")
+
+    assert response.status_code == 200
+    assert response.json() == {"playlists": []}
+    assert response.json() == True  # noqa
+
+    app.dependency_overrides = {}
+
+
 def test_get_all_playlists():
     app.dependency_overrides[PlaylistQueries] = EmptyPlaylistQueries
     response = client.get("/api/playlists")
     assert response.status_code == 200
     assert response.json() == {"playlists": []}
-
-
--- music/tests/test_playlists.py
+++
@@ -1,4 +0,0 @@
-# import os
-# from fastapi.testclient import TestClient
-
-# from routers.playlists import PlaylistIn, Song, PlaylistUpdate
