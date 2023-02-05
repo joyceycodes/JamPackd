bf38f97bcd4cd2b2583378540fe683650cb110a2
Joyce Yu <joyceyu96@gmail.com>
2023-02-04 23:37:44 -0800

    added button to create new playlist

--- ghi/src/nav.js
+++ ghi/src/nav.js
@@ -8,7 +8,6 @@ function Navigation() {
         <ul>
           <NavLink to="/"><img className="JP_logo" src="https://i.imgur.com/HxIEBd3.png" alt="JamPackd Logo"></img></NavLink>
           <LogoutTest />
-
         </ul>
       </div>
       <div className="nav-container">
